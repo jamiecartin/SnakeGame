aa0cc9b44349aec4730a827a734912d721bec89f
jaseyaws <jcartin@ykw.systems>
2023-02-17 15:43:02 -0500

    width, height

--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@ eDirection dir;
 void Setup() {
     gameOver = false;
     dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
 }
 
 void Draw() {
