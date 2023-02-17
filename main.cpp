d888c7d62d6d06e7028fbcf48395703513a95b01
jaseyaws <jcartin@ykw.systems>
2023-02-17 15:10:55 -0500

    outline methods

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 using namespace std;
+bool gameOver;
+
+void Setup() {
+    gameOver = false;
+}
+
+void Draw() {
+
+}
+
+void Input() {
+
+}
+
+void Logic() {
+
+}
+
 int main() {
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+    }
+    
     return 0;
 }
