5b76678325043a09de12dc400e7e77c1e64956ad
jaseyaws <jcartin@ykw.systems>
2023-02-17 15:41:21 -0500

    add dir and start functionality

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
 
 void Setup() {
     gameOver = false;
+    dir = STOP;
 }
 
 void Draw() {
@@ -23,6 +29,9 @@ int main() {
     while (!gameOver)
     {
         Draw();
+        Input();
+        Logic();
+        //Sleep(10); sleep(10);
     }
     
     return 0;
